12341cd4b611390ed5db54aaa149530f9a167a22
carolinemartins <caroline.martins.soares09@gmail.com>
2022-04-11 22:20:07 -0300

    variavel

---
+++ aulavariaveis/index.js
@@ -0,0 +1,22 @@
+ /**/ console.log("Olá Mundo!")
+
+ console.log( prompt("qual seu nome?") )
+  console.log( prompt ("Qual seu sobrenome?"))
+  //
+
+
+  
+ let primeiroNome = "caroline"
+ //
+ let sobrenome = "martins"
+ //
+ let idade = 28
+ //
+ let estudante = true
+ console.log( primeiroNome, sobrenome, idade, estudante )
+
+
+console.log ( typeof idade )
+
+console.log(prompt ("variavel") )
+console.log(prompt ("qual sua idade?"))
