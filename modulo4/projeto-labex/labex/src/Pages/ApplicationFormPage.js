c802dac83e761c744879a2cd189f0c2ef3eaccc7
carolinemartins <caroline.martins.soares09@gmail.com>
2022-08-12 21:33:32 -0300

    api

--- modulo4/projeto-labex/labex/src/Pages/ApplicationFormPage.js
+++ modulo4/projeto-labex/labex/src/Pages/ApplicationFormPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
+import axios from "axios"
 
 
 function FormUsuario (){
@@ -8,9 +9,20 @@ function FormUsuario (){
     const lastPage = () => {
         navigate(-1)
       }
-    
-  
 
+    // const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labeX/caroline-martins-barros/trips/NoIFVcOiSgTKTIPVZwXS/apply'
+    // const candidados=()=>{
+    // const body={
+    //     "name": "",
+    //     "age": Number,
+    //     "applicationText": "",
+    //     "profession": "",
+    //     "country": ""
+    // }
+    //     axios.post(url, body)
+    //     .then((response)=>{url, body, console.log(`resposta ${response.data.trip}`)})
+    //     .catch((erro)=>console.log(erro))}
+        
     return(
         <>
         <p>FormUsuario</p>
