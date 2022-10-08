3a2188324069a6721e7d25aa174e3cdeb56eec0b
carolinemartins <caroline.martins.soares09@gmail.com>
2022-10-08 10:36:56 -0300

    complemento exercicio4

--- modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio4/exercicio.js
+++ modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio4/exercicio.js
@@ -1,25 +1,46 @@
-console.log("exercicio 4");
+
+console.log("exercicio4")
 
 
-// const primeiraLista = [
-// 	{
-// 		nome: "Banana"
-// 	},
-// 	{
-// 		nome: "Laranja"
-// 	}
-// ]
+const primeiraLista = [
+    {
+        nome: "Banana"
+    },
+    {
+        nome: "Laranja"
+    },
+    {
+        nome: "Batata"
+    }
+]
 
-// const segundaLista= [
-// 	{
-// 		nome: "Laranja"
-// 	},
-// 	{
-// 		nome: "Cebola"
-// 	}
-// ]
+const segundaLista = [
+    {
+        nome: "Laranja"
+    },
+    {
+        nome: "Cebola"
+    },
+    {
+        nome: "Cenoura"
+    },
+    {
+        nome: "Batata"
+    }
+]
 
-// function join (oneList, twoList ){
-//     const newList= [...oneList, ...twoList]
-//     console.log(newList)
-// }
+function join(oneList, twoList) {
+    const newList = [...oneList, ...twoList]
+    for (let i = 0; i < newList.length; i++) {
+        let verifica = newList[i].nome
+        for (let j = i + 1 ; j < newList.length; j++) {
+            if (verifica === newList[j].nome) {
+                newList.splice(j, 1)
+                j--
+            }
+        }
+    }
+    return newList
+}
+
+console.log(join(primeiraLista, segundaLista))
