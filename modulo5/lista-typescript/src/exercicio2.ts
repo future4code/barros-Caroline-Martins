0b5c80c3cdfc4a5f24396e91f25e6039a038e06b
carolinemartins <caroline.martins.soares09@gmail.com>
2022-10-20 12:17:37 -0300

    exercicio 2

--- modulo5/lista-typescript/src/exercicio2.ts
+++ modulo5/lista-typescript/src/exercicio2.ts
@@ -1,2 +1,12 @@
 console.log("Exercicio 2");
 
+
+function qualquerCoisa(variavel: any):any{
+    const tipoVariavel= typeof variavel
+
+    return (tipoVariavel)
+
+}
+
+console.log(qualquerCoisa("Carol"));
+console.log(qualquerCoisa(28));
