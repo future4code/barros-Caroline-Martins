90d49a755439f698066c5254d1a1fc874e5375ae
carolinemartins <caroline.martins.soares09@gmail.com>
2022-10-20 20:24:18 -0300

    exercicio 7

---
+++ modulo5/lista-typescript/src/exercicio6.ts
@@ -0,0 +1,20 @@
+console.log("Exercicio 6");
+
+type Clients={
+    cliente: string,
+    saldoTotal:number,
+    debitos:number[]
+}
+
+const ativos: Clients[]=[
+    { cliente: "João", saldoTotal: 1000, debitos: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, debitos: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, debitos: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, debitos: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, debitos: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, debitos: [] }
+]
+
+// function saldoNegativo(list:Clients[]):Clients[]{
+
+// }
