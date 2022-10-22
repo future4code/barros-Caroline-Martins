fbd05cbf3e4b8d7dd1c02d0dcabf53642d082226
carolinemartins <caroline.martins.soares09@gmail.com>
2022-10-22 12:43:39 -0300

    exercico 8 não terminado em loop

--- modulo5/lista-typescript/src/exercicio8.ts
+++ modulo5/lista-typescript/src/exercicio8.ts
@@ -1,2 +1,17 @@
 console.log("Exercicio 8");
 
+// function fatorial(anagrama:string):number{
+//     let palavra: number= anagrama.length
+
+//     if(palavra === 1){
+//         return 1
+//     }else{ for(let i = palavra; i >= 1; i++){
+    //          palavra *= i
+    //         }
+//}
+// return palavra  
+
+// }
+
+// console.log(fatorial("mesa"));
+
