e6796be55abd305169cd82ff08996c3722a68c01
carolinemartins <caroline.martins.soares09@gmail.com>
2022-10-22 13:45:51 -0300

    exercico 8 completo

--- modulo5/lista-typescript/src/exercicio8.ts
+++ modulo5/lista-typescript/src/exercicio8.ts
@@ -1,17 +1,19 @@
 console.log("Exercicio 8");
 
-// function fatorial(anagrama:string):number{
-//     let palavra: number= anagrama.length
+function fatorial(anagrama:string):number{
+    let palavra: number= anagrama.length
 
-//     if(palavra === 1){
-//         return 1
-//     }else{ for(let i = palavra; i >= 1; i++){
-    //          palavra *= i
-    //         }
-//}
-// return palavra  
+    if(palavra === 1){
+        return 1
+    }else{
+        for(let i = palavra -1 ; i > 1; i--){
+                palavra *= i
+               }
+    }
 
-// }
+return palavra  
 
-// console.log(fatorial("mesa"));
+}
+
+console.log(fatorial("mesa"));
 
