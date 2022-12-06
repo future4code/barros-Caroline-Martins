2cdda17985f2845679a0cdccd8313abdd9349e28
carolinemartins <caroline.martins.soares09@gmail.com>
2022-12-06 20:56:35 -0300

    adicionar: get user

--- modulo6/classes-encapsulamento/template-classes-encapsulamento/src/endpoints/getUsers.ts
+++ modulo6/classes-encapsulamento/template-classes-encapsulamento/src/endpoints/getUsers.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from "express"
+import { UserDataBase } from "../class/UserDataBase"
 import connection from "../database/connection"
 import { TABLE_USERS } from "../database/tableNames"
 
 export const getUsers = async (req: Request, res: Response) => {
     let errorCode = 400
     try {
-        // const userDB = new 
-        // const result = await connection(TABLE_USERS).select()
-        // res.status(200).send({ users: result })
+        
+        const userDB = new UserDataBase(connection)
+        
+        const result = await userDB.getUsers()
+
+        res.status(200).send({ users: result })
     } catch (error) {
         res.status(errorCode).send({ message: error.message })
     }
