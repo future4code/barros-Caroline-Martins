4677cfc06a73c0dc8224fe04f23f2da785942d56
carolinemartins <caroline.martins.soares09@gmail.com>
2022-12-06 21:06:47 -0300

    adicionar: class produtc

--- modulo6/classes-encapsulamento/template-classes-encapsulamento/src/endpoints/getUsers.ts
+++ modulo6/classes-encapsulamento/template-classes-encapsulamento/src/endpoints/getUsers.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from "express"
 import { UserDataBase } from "../class/UserDataBase"
 import connection from "../database/connection"
-import { TABLE_USERS } from "../database/tableNames"
 
 export const getUsers = async (req: Request, res: Response) => {
     let errorCode = 400
     try {
         
         const userDB = new UserDataBase(connection)
-        
+
         const result = await userDB.getUsers()
 
         res.status(200).send({ users: result })
