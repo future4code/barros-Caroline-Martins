733e5c8e3996830cad2501c03ad9020c1ce0597a
carolinemartins <caroline.martins.soares09@gmail.com>
2022-12-08 16:50:42 -0300

    adicionar: user purchase

--- modulo6/heranca-encapsulamento/template-heranca/src/database/PurchaseDatabase.ts
+++ modulo6/heranca-encapsulamento/template-heranca/src/database/PurchaseDatabase.ts
@@ -1,5 +1,8 @@
 import { Purchase } from "../models/Purchase";
 import { BaseDatabase } from "./BaseDatabase";
+import { ProductDatabase } from "./ProductDatabase";
+import { UserDatabase } from "./UserDatabase";
+
 
 export class PurchaseDatabase extends BaseDatabase{
     public static TABLE_PURCHASES = "Labe_Purchases"
@@ -12,5 +15,24 @@ export class PurchaseDatabase extends BaseDatabase{
             quantity: purchase.getQuantity(),
             total_price: purchase.getTotalPrice()
         })   
+    }
+
+    public getUserPuchese = async(id:string) => {
+   
+        const [result] = await PurchaseDatabase.connection.raw(`
+        SELECT
+            ${UserDatabase.TABLE_USERS}.email,
+            ${ProductDatabase.TABLE_PRODUCTS}.name AS product_name,
+            ${ProductDatabase.TABLE_PRODUCTS}.price AS product_price,
+            ${PurchaseDatabase.TABLE_PURCHASES}.quantity AS product_quantity,
+            ${PurchaseDatabase.TABLE_PURCHASES}.total_price
+        FROM ${PurchaseDatabase.TABLE_PURCHASES}
+        JOIN ${UserDatabase.TABLE_USERS}
+        ON ${PurchaseDatabase.TABLE_PURCHASES}.user_id = ${UserDatabase.TABLE_USERS}.id
+        JOIN ${ProductDatabase.TABLE_PRODUCTS}
+        ON ${PurchaseDatabase.TABLE_PURCHASES}.product_id = ${ProductDatabase.TABLE_PRODUCTS}.id
+        WHERE ${PurchaseDatabase.TABLE_PURCHASES}.user_id = ${id};
+        `)
+       return result
     }
 }
--- modulo6/heranca-encapsulamento/template-heranca/src/endpoints/getUserPurchases.ts
+++ modulo6/heranca-encapsulamento/template-heranca/src/endpoints/getUserPurchases.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import connection from "../database/connection"
+import { PurchaseDatabase } from "../database/PurchaseDatabase"
 import { TABLE_PRODUCTS, TABLE_PURCHASES, TABLE_USERS } from "../database/tableNames"
 
 export const getUserPurchases = async (req: Request, res: Response) => {
@@ -7,22 +8,26 @@ export const getUserPurchases = async (req: Request, res: Response) => {
     try {
         const id = req.params.id
 
-        const [result] = await connection.raw(`
-        SELECT
-            ${TABLE_USERS}.email,
-            ${TABLE_PRODUCTS}.name AS product_name,
-            ${TABLE_PRODUCTS}.price AS product_price,
-            ${TABLE_PURCHASES}.quantity AS product_quantity,
-            ${TABLE_PURCHASES}.total_price
-        FROM ${TABLE_PURCHASES}
-        JOIN ${TABLE_USERS}
-        ON ${TABLE_PURCHASES}.user_id = ${TABLE_USERS}.id
-        JOIN ${TABLE_PRODUCTS}
-        ON ${TABLE_PURCHASES}.product_id = ${TABLE_PRODUCTS}.id
-        WHERE ${TABLE_PURCHASES}.user_id = ${id};
-        `)
+        // const [result] = await connection.raw(`
+        // SELECT
+        //     ${TABLE_USERS}.email,
+        //     ${TABLE_PRODUCTS}.name AS product_name,
+        //     ${TABLE_PRODUCTS}.price AS product_price,
+        //     ${TABLE_PURCHASES}.quantity AS product_quantity,
+        //     ${TABLE_PURCHASES}.total_price
+        // FROM ${TABLE_PURCHASES}
+        // JOIN ${TABLE_USERS}
+        // ON ${TABLE_PURCHASES}.user_id = ${TABLE_USERS}.id
+        // JOIN ${TABLE_PRODUCTS}
+        // ON ${TABLE_PURCHASES}.product_id = ${TABLE_PRODUCTS}.id
+        // WHERE ${TABLE_PURCHASES}.user_id = ${id};
+        // `)
+        const purchaseDB = new PurchaseDatabase()
 
-        res.status(200).send({ purchases: result })
+        const result = await purchaseDB.getUserPuchese(id)
+
+        res.status(200).send(result)
+        
     } catch (error) {
         res.status(errorCode).send({ message: error.message })
     }
