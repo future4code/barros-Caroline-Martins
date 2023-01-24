b627652ce528ab697d10efdacf5eb409b7b037f9
carolinemartins <caroline.martins.soares09@gmail.com>
2023-01-24 11:31:27 -0300

    user controller

--- modulo6/arquitetura-software-3/labeflix/src/controller/UserController.ts
+++ modulo6/arquitetura-software-3/labeflix/src/controller/UserController.ts
@@ -1,33 +1,39 @@
 import { Request, Response } from "express";
 import { UserBusiness } from "../business/UserBusiness";
+import { UserInputDTO } from "../model/userDTO";
 
 export class UserController {
-  async create(req: Request, res: Response):Promise<void> {
+  async create(req: Request, res: Response): Promise<void> {
     try {
       const { email, name, password } = req.body;
 
+      const input: UserInputDTO = {
+        email: email,
+        name: name,
+        password: password
+      }
       const userBusiness = new UserBusiness();
-      
-      await userBusiness.create({ email, name, password });
+
+      await userBusiness.create(input);
 
       res.status(201).send({ message: "Usuário cadastrado com sucesso" });
     } catch (error: any) {
-      res.status(400).send(error.message);
+      res.status(error.statusCode || 400).send(error.message || error.sqlMessage)
     }
   }
 
-  getAllUser = async (req:Request, res:Response)=>{
-      try{
+  getAllUser = async (req: Request, res: Response) => {
+    try {
 
-        const userBusiness = new UserBusiness()
+      const userBusiness = new UserBusiness()
 
-        const users = await userBusiness.getAllUser()
+      const users = await userBusiness.getAllUser()
 
-        res.status(200).send(users)
+      res.status(200).send(users)
 
-      }catch(e:any){
-        res.status(400).send(e.message)
-      }
+    } catch (e: any) {
+      res.status(e.statusCode || 400).send(e.message || e.sqlMessage)
+    }
 
   }
 }
