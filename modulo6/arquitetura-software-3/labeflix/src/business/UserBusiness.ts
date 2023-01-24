21d57370e96e462e059af84c32d5b307a34d0213
carolinemartins <caroline.martins.soares09@gmail.com>
2023-01-24 11:27:04 -0300

    custom error user business

--- modulo6/arquitetura-software-3/labeflix/src/business/UserBusiness.ts
+++ modulo6/arquitetura-software-3/labeflix/src/business/UserBusiness.ts
@@ -1,18 +1,19 @@
 import { UserDatabase } from "../data/UserDatabase"
 import { v4 as generateId } from 'uuid'
 import { UserInputDTO, UserInsertDTO } from "../model/userDTO"
+import { CustomError } from "../error/CustomError"
 
 export class UserBusiness {
   async create({ email, name, password }: UserInputDTO): Promise<void> {
     if (!email || !name || !password) {
-      throw new Error("Dados inválidos (email, name, password)")
+      throw new CustomError(400, "Dados inválidos (email, name, password)")
     }
 
     const id = generateId()
 
     const userDatabase = new UserDatabase()
 
-    const createUser:UserInsertDTO={
+    const createUser: UserInsertDTO = {
       id,
       name,
       email,
@@ -23,16 +24,13 @@ export class UserBusiness {
 
   getAllUser = async () => {
     try {
-
-
       const userDatabase = new UserDatabase()
 
-     const users = await userDatabase.getAllUser()
-     return(users)
+      const users = await userDatabase.getAllUser()
+      return (users)
 
     } catch (e: any) {
-      throw new Error(e.message);
-
+      throw new CustomError(e.statusCode, e.message)
     }
   }
 
