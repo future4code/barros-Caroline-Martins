45ab8437bc4f226171fcb08a116b947511957597
carolinemartins <caroline.martins.soares09@gmail.com>
2023-01-24 11:44:47 -0300

    custom error data base

--- modulo6/arquitetura-software-3/labeflix/src/data/MovieDatabase.ts
+++ modulo6/arquitetura-software-3/labeflix/src/data/MovieDatabase.ts
@@ -1,36 +1,37 @@
+import { CustomError } from "../error/CustomError";
 import { BaseDatabase } from "./BaseDatabase";
 
 export class MovieDatabase extends BaseDatabase {
-    private static TABLE_NAME = "LABEFLIX_MOVIE";
+  private static TABLE_NAME = "LABEFLIX_MOVIE";
 
-    create =async ({id,title,description,duration_in_minutes,year_of_release}:any)=>{
-        try{
+  create = async ({ id, title, description, duration_in_minutes, year_of_release }: any) => {
+    try {
 
-              await MovieDatabase.connection
-            .insert({
-                id,
-                title,
-                description,
-                duration_in_minutes,
-                year_of_release,
+      await MovieDatabase.connection
+        .insert({
+          id,
+          title,
+          description,
+          duration_in_minutes,
+          year_of_release,
 
-            }).into(MovieDatabase.TABLE_NAME)
+        }).into(MovieDatabase.TABLE_NAME)
 
-        }catch(err:any){
-            throw new Error(err.message);
-        }
-
+    } catch (err: any) {
+      throw new CustomError(err.statusCode, err.message)
     }
 
-    getAllMovie = async () => {
-        try {
-    
-          const result = await MovieDatabase.connection.select().from(MovieDatabase.TABLE_NAME)
-          return (result)
-    
-        } catch (e: any) {
-          throw new Error(e.message);
-    
-        }
-      }
+  }
+
+  getAllMovie = async () => {
+    try {
+
+      const result = await MovieDatabase.connection.select().from(MovieDatabase.TABLE_NAME)
+      return (result)
+
+    } catch (err: any) {
+      throw new CustomError(err.statusCode, err.message)
+
+    }
+  }
 }
