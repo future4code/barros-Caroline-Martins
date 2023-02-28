289cd4de69378f2966e97b62cc22e36074c5d22e
carolinemartins <caroline.martins.soares09@gmail.com>
2023-02-28 16:38:17 -0300

    testes

--- modulo7/jest-template/tests/purchase.test.ts
+++ modulo7/jest-template/tests/purchase.test.ts
@@ -1,9 +1,9 @@
 import { performPurchase } from "../src/funcs/performPurchase"
 import { User } from "../src/models/User"
 
-describe("Teste User", () => {
+describe("Teste User 1", () => {
 
-    test("Verificar Balance!", () => {
+    test("Verificar Balance maior que Value", () => {
         const user: User = {
             name: "Carol",
             balance: 200
@@ -12,5 +12,38 @@ describe("Teste User", () => {
         const resultSaldo = performPurchase(user, 100)
 
         expect(resultSaldo).toEqual({ name: "Carol", balance: 100 })
+    })
+
+
+
+})
+describe("Teste User 2", () => {
+
+    test("Testing balance greater than value", () => {
+        const user: User = {
+            name: "Astrodev",
+            balance: 50
+        }
+
+        const result = performPurchase(user, 50)
+
+        expect(result).toEqual({
+            ...user,
+            balance: 0
+        })
+    })
+})
+
+describe("Teste User 3", () => {
+
+    test("Testing balance greater than value", () => {
+        const user: User = {
+            name: "Astrodev",
+            balance: 30
+        }
+
+        const result = performPurchase(user, 50)
+
+        expect(result).not.toBeDefined()
     })
 })
